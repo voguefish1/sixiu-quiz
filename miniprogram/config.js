6fef7809eda737f42c0cd84cecb94783adbe0ef8
voguefish1 <862025867@qq.com>
2026-06-28 00:46:40 +0800

    Fix: 使用jsdelivr CDN加速题库加载

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -3,5 +3,6 @@ module.exports = {
     { file: 'questions.json', name: '思想道德与法治', desc: '992题' },
     { file: 'banks/jindaishi.json', name: '中国近代史', desc: '116题' }
   ],
-  baseUrl: 'https://voguefish1.github.io/sixiu-quiz/'
+  // jsDelivr CDN 比 GitHub Pages 快
+  baseUrl: 'https://cdn.jsdelivr.net/gh/Vizards/sixiu-quiz@main/'
 };
