822684aa0161f4322d75fcaa4d4f6bec5f5a3b15
voguefish1 <862025867@qq.com>
2026-06-28 19:30:50 +0800

    Fix: 近代史题库所有题目缺失ID导致答案互相覆盖

---
+++ fix_ids.py
@@ -0,0 +1,9 @@
+﻿import json
+with open(r'C:\temp\sixiu-quiz\banks\jindaishi.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+qs = data.get('questions', data)
+for i, q in enumerate(qs):
+    q['id'] = str(i + 1)
+with open(r'C:\temp\sixiu-quiz\banks\jindaishi.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False)
+print('Done. Count:', len(qs), 'First IDs:', qs[0]['id'], qs[1]['id'], qs[2]['id'])
